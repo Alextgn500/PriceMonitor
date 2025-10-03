f2f80677bf8d19ba1e6ff10127832efa35a64b4c
Александр <azubkov500@gmail.com>
2025-10-03 13:20:58 +0300

    Update test_aiogram.py
    Удален секретный бот-токен

--- test_aiogram.py
+++ test_aiogram.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot
 
 async def test_bot():
-    token = '8355450140:AAEPFTyVSx3YhhgL1XRnKCu8Guslv6_3Fbw'
+    token = ''
     try:
         bot = Bot(token=token)
         me = await bot.get_me()
